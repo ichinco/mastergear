90cc418548514f3bd553f1a9b7299f721c6253ab
Denise Ichinco <ichinco@gmail.com>
2013-06-02 10:08:40 -0400

    add sitemap

--- grails-app/controllers/com/mastergear/CorporateController.groovy
+++ grails-app/controllers/com/mastergear/CorporateController.groovy
@@ -4,7 +4,48 @@ class CorporateController {
 
     def index() {}
 
-    def about(){
-        render view:"about"
+    def about() {
+        render view: "about"
+    }
+
+    def sitemap() {
+        render(contentType: 'text/xml', encoding: 'UTF-8') {
+            mkp.yieldUnescaped '<?xml version="1.0" encoding="UTF-8"?>'
+            urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+                    'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+                    'xsi:schemaLocation': "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd") {
+                url {
+                    loc(g.createLink(absolute: true, controller: 'gearList', action: 'index'))
+                    changefreq('hourly')
+                    priority(1.0)
+                }
+                url {
+                    loc(g.createLink(absolute: true, controller: 'trail', action: 'list'))
+                    changefreq('hourly')
+                    priority(1.0)
+                }
+                GearList.list().each { domain ->
+                    url {
+                        loc(g.createLink(absolute: true, controller: 'gearList', action: 'showList', id: domain.id))
+                        changefreq('hourly')
+                        priority(0.8)
+                    }
+                }
+                GearUser.list().each { domain ->
+                    url {
+                        loc(g.createLink(absolute: true, controller: 'user', action: 'show', params: [userId: domain.id]))
+                        changefreq('hourly')
+                        priority(0.8)
+                    }
+                }
+                Trail.list().each { domain ->
+                    url {
+                        loc(g.createLink(absolute: true, controller: 'trail', action: 'show', id: domain.id))
+                        changefreq('hourly')
+                        priority(0.8)
+                    }
+                }
+            }
+        }
     }
 }
---
+++ grails-app/views/corporate/sitemap.gsp
@@ -0,0 +1,14 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: denise
+  Date: 6/2/13
+  Time: 9:53 AM
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<body>
+
+</body>
+</html>
