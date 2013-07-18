fc0915a13b0eb8797fa5718716e92427387e0207
Denise Ichinco <ichinco@gmail.com>
2013-07-17 20:19:47 -0400

    fix trail url

--- web-app/js/start/collections/possible-trail-collection.js
+++ web-app/js/start/collections/possible-trail-collection.js
@@ -7,7 +7,7 @@ Mastergear.Collection.PossibleTrails = Backbone.Collection.extend({
 
     initialize : function(){
         this.model = Mastergear.Models.Trail;
-        this.url = "/mastergear/trail/"
+        this.url = "trail/"
     },
 
     setSelected : function(trail){
