9ca82346dfba4e5f0edab658a3c5ba26308bc3fe
Knight Fu <knight.w.fu@gmail.com>
2013-07-20 16:15:57 -0400

    just to see what she made, I will commit these unfinished files to the repo; I really should think of a better way to deal with this (again)

---
+++ test/unit/com/mastergear/GeocodingServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.mastergear
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(GeocodingService)
+class GeocodingServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
---
+++ web-app/js/initialize-list/views/trail-query-view.js
@@ -0,0 +1,18 @@
+var Mastergear = Mastergear || {};
+Mastergear.Views = Mastergear.Views || {};
+
+Mastergear.Views.TrailQuery = Backbone.View.extend({
+    events : {
+        'submit .search-form' : 'queryTrails'
+    },
+
+    initialize : function() {
+        _.bindAll(this);
+    },
+
+    queryTrails : function(evt) {
+        evt.preventDefault();
+        this.model.setQuery(this.$el.find(".category-desc").val());
+        return false;
+    }
+});
